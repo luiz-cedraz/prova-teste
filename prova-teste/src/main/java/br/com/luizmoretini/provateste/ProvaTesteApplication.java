a9df46f5248be7b816c626358fc3b39a516e6fb7
Luiz Henrique Cedraz <95879662+luiz-cedraz@users.noreply.github.com>
2022-03-16 19:40:42 -0300

    Add files via upload

---
+++ prova-teste/src/main/java/br/com/luizmoretini/provateste/ProvaTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizmoretini.provateste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaTesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProvaTesteApplication.class, args);
+    }
+
+}
---
+++ prova-teste/src/test/java/br/com/luizmoretini/provateste/ProvaTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizmoretini.provateste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaTesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
